2411dd2d4363e8e4a211ce6d69ff26ff68295ff9
Kamila Luz <146234680+kamilaluz@users.noreply.github.com>
2024-11-23 18:27:31 -0300

    Criação das classes

---
+++ DadosUniversitarios/Models/Disciplina.cs
@@ -0,0 +1,8 @@
+﻿namespace DadosUniversitarios.Models
+{
+    public class Disciplina
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ DadosUniversitarios/Models/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿namespace DadosUniversitarios.Models
+{
+    public class Fornecedor : Empresa
+    {
+        public int Id { get; set; }
+        public int NumeroContrato { get; set; }
+        public int Periodicidade { get; set; }
+        public double ValorServico { get; set; }
+        public DateOnly DataPagamento { get; set; }
+        public DateOnly VencimentoContrato { get; set; }
+
+    }
+}
